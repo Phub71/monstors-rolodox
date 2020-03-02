7d3f94bec80e7428113f70de5d7863a83ecdcf16
poba <pooriya.balavi@netlight.com>
2020-03-02 13:35:33 +0100

    Updates

---
+++ precache-manifest.9c560da000bb4096a89877d5e6531623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795230cff48d251ce43e8cd44fbb00bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab6c70592f0cce2eb37",
+    "url": "/static/css/main.f51fab1e.chunk.css"
+  },
+  {
+    "revision": "f18ec448c3ab67b3b179",
+    "url": "/static/js/2.9e4b70aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9e4b70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab6c70592f0cce2eb37",
+    "url": "/static/js/main.a00761eb.chunk.js"
+  },
+  {
+    "revision": "95b00f7e967302e9ee68",
+    "url": "/static/js/runtime-main.7873e549.js"
+  }
+]);
